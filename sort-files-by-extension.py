6ab9901779430800243233b8a719e539cc1aba94
ZakYeo <zakyeomanson@gmail.com>
2023-02-01 15:23:45 +0000

    Now safely handles duplicates instead of overwriting

--- sort-files-by-extension.py
+++ sort-files-by-extension.py
@@ -36,12 +36,42 @@ def loop_copy(old_path, new_path, shouldExtract=True, move=False):
             # File is not zip or rar, so handle like a normal file
             # Move the file or copy the file depending on parameters
             if move:
-                shutil.move(filepath, new_dir)
+                safe_copy_or_move(filepath, new_dir, copy=False)
             else:
-                shutil.copy(filepath, new_dir)
+                safe_copy_or_move(filepath, new_dir, copy=True)
     # Delete temp folder if it exists
     if(os.path.exists(temp_path)):
         shutil.rmtree(temp_path)
+
+
+def safe_copy_or_move(file_path, out_dir, copy=True, dst=None):
+    """Safely copy or move a file to the specified directory. If a file with the same name already 
+    exists, the copied file name is altered to preserve both.
+
+    :param str file_path: Path to the file to copy.
+    :param str out_dir: Directory to copy the file into.
+    :param str dst: New name for the copied file. If None, use the name of the original
+        file.
+    """
+    # This lovely piece of code modified from:
+    # https://stackoverflow.com/questions/33282647/python-shutil-copy-if-i-have-a-duplicate-file-will-it-copy-to-new-location
+    name = dst or os.path.basename(file_path)
+    if not os.path.exists(os.path.join(out_dir, name)):
+        if copy:
+            shutil.copy(file_path, os.path.join(out_dir, name))
+        else:
+            shutil.move(file_path, os.path.join(out_dir, name))
+    else:
+        base, extension = os.path.splitext(name)
+        i = 1
+        while os.path.exists(os.path.join(out_dir, '{}_{}{}'.format(base, i, extension))):
+            i += 1
+        if copy:
+            shutil.copy(file_path, os.path.join(
+                out_dir, '{}_{}{}'.format(base, i, extension)))
+        else:
+            shutil.move(file_path, os.path.join(
+                out_dir, '{}_{}{}'.format(base, i, extension)))
 
 
 def handle_rar(new_path, filepath, temp_path, move=True):
